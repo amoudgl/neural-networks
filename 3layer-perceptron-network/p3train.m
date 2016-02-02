f5e155aab21404dd00b6151b5e85e5874bb94aef
abhinavmoudgil95 <abhinavmoudgil95@gmail.com>
2016-02-02 14:08:42 +0530

    Simple NN for handwritten digit recognition

---
+++ 3layer-perceptron-network/p3train.m
@@ -0,0 +1,82 @@
+% Neural network for handwritten digit classification
+% Model of neural network - 64-32-2
+
+clear;
+clc;
+close all;
+
+tic 
+
+filename = 'train.txt';
+X = dlmread(filename, ',');
+n = size(X, 1); % Number of training samples 
+t = X(:, 65);
+X = X(:, 1 : 64);
+X = X';
+
+%Normalization of input
+for i = 1 : 64
+    if (sum(X(i, :)) ~= 0)
+    X(i, :) = X(i, :) / max(X(i, :));
+    end
+end
+
+% Initialization by feedforward operation
+
+%bias1 = 1;
+%bias2 = 1;
+
+f = @(x) logsig(x);
+df = @(x) logsig(x) .* (1 - logsig(x));
+
+a = -1/sqrt(size(X, 1));
+b = 1/sqrt(size(X, 1));
+wji = (b - a) .* rand(32, size(X, 1)) + a;
+netj = wji * X;
+yj = f(netj);
+%yj(33, :) = 1;
+a = -1/sqrt(size(netj, 1));
+b = 1/sqrt(size(netj, 1));
+wkj = (b - a) .* rand(2, size(yj, 1)) + a;
+netk = wkj * yj;
+zk = f(netk);
+
+% Backpropagation operation
+tk(:, 1) = t < 6;
+tk(:, 2) = t > 6;
+tk = tk';
+Jw = 0.5 * sum((tk - zk) .^ 2); % Gives error for each training sample in each column
+delk = (tk - zk) .* df(netk);
+delj = df(netj) .* (wkj' * delk);
+
+eta = 1;
+k = 0;
+while k < n 
+     k = mod(k, n) + 1; %Kth training sample
+     xk = X(:, k);
+     netj(:, k) = wji * X(:, k);
+     yj(:, k) = f(netj(:, k));
+     netk(:, k) = wkj * yj(:, k);
+     zk(:, k) = f(netk(:, k));
+     delk(:, k) = (tk(:, k) - zk(:, k)) .* df(netk(:, k));
+    % delj(:, k) = df(netj(:, k)) * delk(:, k)' * wkj(:, k);
+     delj(:, k) = df(netj(:, k)) .* (wkj' * delk(:, k));
+     wkj = wkj + eta * delk(:, k) * yj(:, k)';
+     wji = wji + eta * delj(:, k) * xk';
+     
+     % Updating all values according to new weights 
+     netj = wji * X;
+     yj = logsig(netj);
+     netk = wkj * yj;
+     zk = f(netk);
+     
+     % Error 
+     Jw(k) = 0.5 * sum((tk(:, k) - zk(:, k)) .^ 2);   
+end
+
+zk = zk > 0.5;
+err = tk - zk;
+numberofErrors = sum(max(err ~= 0))
+accuracy = (size(X, 2) - (numberofErrors))/size(X, 2)
+
+toc
